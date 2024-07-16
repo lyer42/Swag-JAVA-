7f224a385860790c50e21a7ee79a9d6eb40cd7fb
Lyer42 <senamostaev@gmail.com>
2024-07-16 13:44:20 +0300

    я что то норм написал!(

--- main.java
+++ main.java
@@ -1,5 +1,13 @@
 public class main {
 
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
 
-    System.out.println("Я ЛОХ Я ЛОХ ЛОХ ЛОХ");
+        System.out.println(sum);
+
+
+    }
 }
+
